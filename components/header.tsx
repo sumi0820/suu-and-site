0f374d0161e315ca22a5ed6b262b75dc8af3fae8
Sumiya Ushiro <48553998+sumi0820@users.noreply.github.com>
2023-07-10 23:14:18 +0900

    Added favicon

--- components/header.tsx
+++ components/header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
     />
   );
   return (
-    <section className="flex justify-between items-center mt-8 top-0 z-50 sticky opacity-80 bg-white px-8 pt-4">
+    <section className="flex justify-between items-center top-0 z-50 sticky opacity-80 bg-white px-8 pt-4">
       <Link href={"/"}> {logo}</Link>
       <div className="flex gap-8">
         <Link className="link-text" href={"/#about"}>ABOUT</Link>
